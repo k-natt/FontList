c08fd0ba18619c41177d1779f38ab7dc3fe59619
Kevin Nattinger <kevin@nattinger.net>
2017-04-18 21:57:19 -0700

    Modernize

--- FontList/FontList/FontListViewController.m
+++ FontList/FontList/FontListViewController.m
@@ -10,44 +10,46 @@
 
 @interface FontListViewController ()
 
-@property (nonatomic, strong) NSArray *families;
-@property (nonatomic, strong) NSDictionary *names;
-@property (nonatomic) int fontsz;
+@property (nonatomic, strong) NSArray<NSString *> *families;
+@property (nonatomic, strong) NSDictionary<NSString *, NSArray<NSString *> *> *names;
+@property (nonatomic) CGFloat fontSize;
 
 @end
 
+static NSString *reuseIdentifier = @"FontCell";
+
 @implementation FontListViewController
 
--(id) init
+- (instancetype)init
 {
     return [self initWithStyle:UITableViewStylePlain];
 }
 
-- (id)initWithStyle:(UITableViewStyle)style
+- (instancetype)initWithStyle:(UITableViewStyle)style
 {
     self = [super initWithStyle:style];
     if (self) {
-        self.families = [[UIFont familyNames] sortedArrayUsingSelector:@selector(compare:)];
-        NSMutableDictionary *fonts = [NSMutableDictionary dictionary];
-        for(NSString *family in self.families) {
+        self.families = [[UIFont familyNames] sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+        NSMutableDictionary<NSString *, NSArray<NSString *> *> *fonts = [NSMutableDictionary dictionary];
+        for (NSString *family in self.families) {
             fonts[family] = [UIFont fontNamesForFamilyName:family];
         }
-        self.fontsz = 18;
+        self.fontSize = 18;
         self.names = fonts;
     }
     return self;
 }
 
--(void) setFontsz:(int)fontsz
+- (void)setFontSize:(CGFloat)fontSize
 {
-    _fontsz = fontsz;
-    self.title = [NSString stringWithFormat:@"Size %d", fontsz];
-    if(self.isViewLoaded) {
+    _fontSize = fontSize;
+    self.title = [NSString stringWithFormat:@"Size %d", (int)fontSize];
+    if (self.isViewLoaded) {
         [self.tableView reloadData];
     }
 }
 
--(void) viewDidLoad
+- (void)viewDidLoad
 {
     [super viewDidLoad];
 
@@ -62,34 +64,34 @@
     self.navigationItem.leftBarButtonItem  = dn;
     self.navigationItem.rightBarButtonItem = up;
 
+    self.tableView.estimatedRowHeight = 44;
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:reuseIdentifier];
 }
 
--(void) bigger { self.fontsz++; }
--(void) smaller{ self.fontsz--; }
+- (void)bigger { self.fontSize++; }
+- (void)smaller { self.fontSize--; }
 
--(int) numberOfSectionsInTableView:(UITableView *)tableView
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     return self.families.count;
 }
--(int) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     return [self.names[self.families[section]] count];
 }
--(UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *rid = @"FontCell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:rid];
-    if(!cell) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
-                                      reuseIdentifier:rid];
-    }
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:reuseIdentifier forIndexPath:indexPath];
     NSString *font = self.names[self.families[indexPath.section]][indexPath.row];
-    cell.textLabel.font = [UIFont fontWithName:font size:self.fontsz];
+    cell.textLabel.font = [UIFont fontWithName:font size:self.fontSize];
     cell.textLabel.text = font;
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
     return cell;
 }
 
--(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
 {
     return self.families[section];
 }
